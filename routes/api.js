df9e9bff309b63f916a49bcbaabafab4dc45f65d
김범진 <sdpthf@naver.com>
2021-04-06 20:05:06 +0900

    [MODIFY] api.js Add Push Array Router

--- routes/api.js
+++ routes/api.js
@@ -5,10 +5,23 @@ var arr = ["Hello", "world", "test", "string"];
 
 
 /* GET home page. */
-router.get('/:name', function(req, res, next) {
-  console.log(req.params.name);
-    console.log(arr[+req.params.name]);
+router.get('/', function(req, res, next) {
+   res.send("hello world");
+});
+
+router.get('/arr', function(req, res, next) {
+    console.log(arr);
     res.json(arr);
+});
+
+router.get('/:name', function(req, res, next) {
+    console.log(arr[+req.params.name]);
+    res.send(arr[+req.params.name]);
+});
+
+router.get('/add/:name', function(req, res, next) {
+    arr.push(req.params.name);
+    res.send("ok");
 });
 
 module.exports = router;
